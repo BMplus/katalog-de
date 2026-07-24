a40a053f9ed377eb6449587cbfe46ada7966d573
BMplus <malina@bmplus.cz>
2026-07-24 13:09:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = 'BMplus_katalog_2024_blok_DE.pdf';
+const url = 'BMplus_katalog_DE.pdf';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.12.313/pdf.worker.min.js';
 
